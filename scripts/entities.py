54a066abb6291b35c094e940d09c23d2c50cede9
Sean O'Connor <soc1641@icloud.com>
2025-02-20 12:28:30 -0600

    created alter floor tiles red function

--- scripts/entities.py
+++ scripts/entities.py
@@ -35,13 +35,13 @@ class PhysicsEntity:
 
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1]) #movement function
         entity_mask = self.mask()
+        tileSize = tilemap.tile_size
 
         for rect in tilemap.floor_rects_around(self.pos): #function that changes floor tiles
 
             tile_surface = pygame.Surface(rect.size)
             tile_surface.fill((255, 255, 255))
             wall_mask = pygame.mask.from_surface(tile_surface) # makes a mask of the wall
-            tileSize = tilemap.tile_size
 
             offset = (rect.x - self.pos[0], rect.y - self.pos[1])
             overlap_point = entity_mask.overlap(wall_mask, offset)
@@ -55,22 +55,65 @@ class PhysicsEntity:
                     tile['type'] = 'endb'
                     tile['style'] = 1
                 elif tile['style'] == 0 and tile['type'] != 'start':
-                    if frame_movement[0] > 0:             
+                    if frame_movement[0] > 0 and tile['variant'] != 1 and tile['variant'] != 12 and tile['variant'] != 14: #ensuring that only the right varients get set with the appropriate attributes            
                         tile['type'] = 'circuitb'
                         tile['style'] = 1
                         tile['side'] = 'right' 
-                    if frame_movement[0] < 0:
+                    if frame_movement[0] < 0 and tile['variant'] != 1 and tile['variant'] != 12 and tile['variant'] != 14:
                         tile['type'] = 'circuitb'
                         tile['style'] = 1
                         tile['side'] = 'left'                  
-                    if frame_movement[1] > 0:
+                    if frame_movement[1] > 0 and tile['variant'] != 0 and tile['variant'] != 11 and tile['variant'] != 13:
                         tile['type'] = 'circuitb'
                         tile['style'] = 1
                         tile['side'] = 'down'                   
-                    if frame_movement[1] < 0:
+                    if frame_movement[1] < 0 and tile['variant'] != 0 and tile['variant'] != 11 and tile['variant'] != 13:
                         tile['type'] = 'circuitb'
                         tile['style'] = 1
                         tile['side'] = 'up'
+
+                    #need to add case that can change red tiles back to blue
+    
+
+    #if blue tile has an side of 'right' then that means the right side of the player first made contact with it
+    #in order to transform to red at the correct time the player must be moving Left with only the right side of the player making contact with the sqaure when adding positively to the players x direction
+    def alter_floor_red(self, tilemap, movement=(0,0)): 
+        entity_rect = self.rect()
+        leftEdge = pygame.Rect(entity_rect.left + 1, entity_rect.top + 1, 2, entity_rect.height - 2) #subtracts 2 from HEIGHT
+        rightEdge = pygame.Rect(entity_rect.right - 1, entity_rect.top + 1, 2, entity_rect.height - 2)
+        topEdge = pygame.Rect(entity_rect.left + 1, entity_rect.top + 1, entity_rect.width - 2, 2)
+        bottomEdge = pygame.Rect(entity_rect.left + 1, entity_rect.bottom - 1, entity_rect.width - 2, 2)
+        tileSize = tilemap.tile_size
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        for rect in tilemap.floor_rects_around(self.pos):
+
+            rectPos = rect.topleft
+            tile_loc = (int((rectPos[0]) // tileSize), int((rectPos[1]) // tileSize))
+            check_loc = str(tile_loc[0]) + ';' + str(tile_loc[1])
+            tile = tilemap.get_tile(check_loc)
+            if tile['style'] == 1:
+                if frame_movement[0] > 0:  #right
+                    if tile['side'] == 'left':
+                        if leftEdge.colliderect(rect) and not rightEdge.colliderect(rect):
+                            tile['type'] = 'circuitr'
+                            tile['style'] = 2 
+                if frame_movement[0] < 0: #left
+                    if tile['side'] == 'right':
+                        if rightEdge.colliderect(rect) and not leftEdge.colliderect(rect):
+                            tile['type'] = 'circuitr'
+                            tile['style'] = 2              
+                if frame_movement[1] > 0: #down
+                    if tile['side'] == 'up':
+                        if topEdge.colliderect(rect) and not bottomEdge.colliderect(rect):
+                            tile['type'] = 'circuitr'
+                            tile['style'] = 2                
+                if frame_movement[1] < 0: #up
+                    if tile['side'] == 'down':
+                        if bottomEdge.colliderect(rect) and not topEdge.colliderect(rect):
+                            tile['type'] = 'circuitr'
+                            tile['style'] = 2 
+
 
 
     def update(self, tilemap, movement=(0,0)): #function for calculating the movement of the player
@@ -111,6 +154,8 @@ class PhysicsEntity:
                     self.pos[1] -= frame_movement[1]
 
             self.alter_floor_blue(tilemap, movement)
+
+            self.alter_floor_red(tilemap, movement)
 
             # would need to remove lower portion of this function if we add enemy entities since the following is just for the player
             for rect in tilemap.at_end(self.pos):
